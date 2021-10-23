95971a3e149d8c3d3b27da28debeaac39c859704
PastelWhale <43264739+HongDaeEui@users.noreply.github.com>
2021-10-23 20:37:39 +0900

    Add: implement static website

--- node.js/board project/index.js
+++ node.js/board project/index.js
@@ -1,5 +1,9 @@
 var express = require("express");
-var mongoose = require("mongoose");
-var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
 var app = express();
+
+app.use(express.static(__dirname + "/public")); // 1
+
+var port = 3000;
+app.listen(port, function () {
+  console.log("server on! http://localhost:" + port);
+});
